35a8db2b39d443fa856e2cbb2843fe0633ea1c0d
Revocod3 <kevinlopez2693@gmail.com>
2022-07-20 17:23:35 -0400

    add: min&max area filters

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -67,6 +67,8 @@ class PropertiesController <ApplicationController
             :about,
             :max_price,
             :min_price,
+            :min_area,
+            :max_area
         )
     end  
     
